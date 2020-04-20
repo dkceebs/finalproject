7f43763e0200b0406300ad23f5f39931439978fe
Daiki Kuroda <dk.ceebs@gmail.com>
2020-04-20 22:33:22 +0900

    hey

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+var port = process.env.PORT || 3000;
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -13,7 +14,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-
+app.use(express.json())
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -22,6 +23,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://user:1031daiki@ds119640.mlab.com:19640/heroku_8xth0xzm", {
   useNewUrlParser: true
 });
+mongoose.connect('mongodb://localhost/node-express-mongodb-heroku');
 
 const postSchema = {
   title: String,
@@ -85,6 +87,5 @@ app.get("/contact", function (req, res) {
   });
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000");
-});
+app.listen(port);
+console.log('listen on port ' + port);
